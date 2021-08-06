7b08ff87fe10d8ebb04b333dccda615f6c333b00
Tommaso Fonda <fondatommaso2@gmail.com>
2021-08-06 15:13:11 +0200

    Main menu with input fields and two buttons

--- src/main/java/it/units/italiandraughts/ItalianDraughts.java
+++ src/main/java/it/units/italiandraughts/ItalianDraughts.java
@@ -14,7 +14,7 @@ public class ItalianDraughts extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(ItalianDraughts.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 400, 400);
+        Scene scene = new Scene(fxmlLoader.load(), 350, 240);
         stage.setTitle("ItalianDraughts");
         stage.setScene(scene);
         primaryStage = stage;
