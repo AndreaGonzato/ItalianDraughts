aa054065f71cc84270bb6c4b6c077b935947c8c8
Andrea Gonzato <gonzatoandrea98@gmail.com>
2021-08-06 16:48:51 +0200

    add file player

---
+++ src/main/java/it/units/italiandraughts/logic/Player.java
@@ -0,0 +1,12 @@
+package it.units.italiandraughts.logic;
+
+public class Player {
+    private String name;
+    private int score;
+
+    Player(String name){
+        this.name = name;
+        score = 0;
+    }
+    
+}
