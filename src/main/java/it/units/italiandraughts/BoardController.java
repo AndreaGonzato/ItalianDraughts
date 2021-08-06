6e41f84df6bbe022d62b56365015f6d0cece90ec
Andrea Gonzato <gonzatoandrea98@gmail.com>
2021-08-06 16:11:03 +0200

    add board

--- src/main/java/it/units/italiandraughts/BoardController.java
+++ src/main/java/it/units/italiandraughts/BoardController.java
@@ -1,12 +1,37 @@
 package it.units.italiandraughts;
 
 import javafx.fxml.FXML;
-import javafx.scene.layout.Pane;
+import javafx.geometry.HPos;
+import javafx.geometry.VPos;
+import javafx.scene.Scene;
+import javafx.scene.control.Control;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
 
 public class BoardController {
     @FXML
-    Pane board;
+    GridPane board;
 
     public void initialize() {
+        final int size = 8;
+        for (int row = 0; row < size; row++) {
+            for (int col = 0; col < size; col++) {
+                StackPane square = new StackPane();
+                String color;
+                if ((row + col) % 2 == 0) {
+                    color = "white";
+                } else {
+                    color = "black";
+                }
+                square.setStyle("-fx-background-color: " + color + ";");
+                board.add(square, col, row);
+            }
+        }
+        for (int i = 0; i < size; i++) {
+            board.getColumnConstraints().add(new ColumnConstraints(5, Control.USE_COMPUTED_SIZE, Double.POSITIVE_INFINITY, Priority.ALWAYS, HPos.CENTER, true));
+            board.getRowConstraints().add(new RowConstraints(5, Control.USE_COMPUTED_SIZE, Double.POSITIVE_INFINITY, Priority.ALWAYS, VPos.CENTER, true));
+        }
+
+
     }
 }
