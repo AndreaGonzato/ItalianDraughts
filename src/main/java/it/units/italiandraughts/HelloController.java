1f029449a866ea58308dbee217a490ed2cd682de
Tommaso Fonda <fondatommaso2@gmail.com>
2021-08-06 14:42:22 +0200

    Add Start & Exit buttons

--- src/main/java/it/units/italiandraughts/HelloController.java
+++ src/main/java/it/units/italiandraughts/HelloController.java
@@ -1,14 +1,25 @@
 package it.units.italiandraughts;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.scene.Group;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    protected void startGame() {
+        Group group = new Group();
+        Scene scene = new Scene(group, 320, 240);
+        Label text = new Label("TEST TEXT");
+        text.setLayoutX(20);
+        group.getChildren().addAll(text);
+        ItalianDraughts.getStage().setScene(scene);
+    }
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void quitGame() {
+        Platform.exit();
+        System.exit(0);
     }
 }
--- src/main/java/it/units/italiandraughts/ItalianDraughts.java
+++ src/main/java/it/units/italiandraughts/ItalianDraughts.java
@@ -2,23 +2,35 @@ package it.units.italiandraughts;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class ItalianDraughts extends Application {
+
+    private static Stage primaryStage;
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(ItalianDraughts.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 400, 400);
         stage.setTitle("Hello!");
         stage.setScene(scene);
+        primaryStage = stage;
         stage.show();
     }
 
     public static void main(String[] args) {
         launch();
+    }
+
+    protected static Stage getStage() {
+        return primaryStage;
     }
 
     public static int addOne(int n) {
