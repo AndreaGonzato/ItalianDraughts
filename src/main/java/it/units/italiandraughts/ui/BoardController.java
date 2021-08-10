b411f7c21adb8dc52b5b6491996d8cc4560358fb
Tommaso Fonda <fondatommaso2@gmail.com>
2021-08-10 12:11:43 +0200

    ui: add column labels

--- src/main/java/it/units/italiandraughts/ui/BoardController.java
+++ src/main/java/it/units/italiandraughts/ui/BoardController.java
@@ -2,10 +2,14 @@ package it.units.italiandraughts.ui;
 
 import it.units.italiandraughts.ItalianDraughts;
 import it.units.italiandraughts.logic.Board;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.layout.*;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 import java.util.List;
@@ -19,10 +23,12 @@ public class BoardController {
     VBox rowNumbers;
 
     @FXML
+    HBox columnLetters;
+
+    @FXML
     Label player1Name, player2Name;
 
     public void initialize() {
-
         BoardDisplayer boardDisplayer = new BoardDisplayer(gridPane);
         Board board = new Board(boardDisplayer);
         board.draw();
@@ -31,20 +37,23 @@ public class BoardController {
         stage.setMinHeight(stage.getHeight());
         stage.setMinWidth(stage.getWidth());
 
-        // add numbers left to the board
+        // resize the numbers to the left of board
         List<Node> rowLabels = rowNumbers.getChildren();
         rowLabels.forEach(e -> {
             ((Label) e).setMaxHeight((ItalianDraughts.getScreenHeight() / 3 * 2) / 8);
             ((Label) e).setMinHeight((ItalianDraughts.getScreenHeight()/3*2)/8);
         });
 
-        System.out.println(board); // TODO TEST
+        Platform.runLater(() -> columnLetters.setPadding(new Insets(0, 0, 0,
+                rowNumbers.getWidth())));
 
-
-
-
-
-
+        // resize the letters under the board
+        List<Node> columnLabels = columnLetters.getChildren();
+        columnLabels.forEach(e -> {
+            ((Label) e).setMaxWidth((ItalianDraughts.getScreenHeight() / 3 * 2) / 8);
+            ((Label) e).setMinWidth((ItalianDraughts.getScreenHeight() / 3 * 2) / 8);
+            ((Label) e).setAlignment(Pos.CENTER);
+        });
     }
 
 }
