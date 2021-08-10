05aae46320a1546e75d4ddca39e76bff8732a59b
Andrea Gonzato <gonzatoandrea98@gmail.com>
2021-08-10 11:02:46 +0200

    new class ui.board

---
+++ src/main/java/it/units/italiandraughts/ui/Board.java
@@ -0,0 +1,58 @@
+package it.units.italiandraughts.ui;
+
+import it.units.italiandraughts.ItalianDraughts;
+import it.units.italiandraughts.logic.Piece;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
+import javafx.scene.layout.StackPane;
+
+public class Board {
+    public Board(GridPane board) {
+        board.setMinSize(
+                ItalianDraughts.getScreenHeight()/3*2,
+                ItalianDraughts.getScreenHeight()/3*2
+        );
+        board.setMaxSize(
+                ItalianDraughts.getScreenHeight()/3*2,
+                ItalianDraughts.getScreenHeight()/3*2
+        );
+
+        final int size = 8;
+        for (int row = 0; row < size; row++) {
+            for (int col = 0; col < size; col++) {
+                StackPane square = new StackPane();
+                String color;
+                if ((row + col) % 2 == 0) {
+                    color = "#d47d35";
+                } else {
+                    color = "white";
+                }
+
+                // TODO just a test, try to add a single piece
+                if (col == 4 && row == 2){
+                    Piece piece = new Piece(board.getMaxHeight()/8);
+                    square.getChildren().addAll(piece.getBaseEllipse());
+                    square.getChildren().add(piece.getUpperEllipse());
+                }
+
+
+
+                square.setStyle("-fx-background-color: " + color + ";");
+                board.add(square, col, row);
+            }
+        }
+
+
+        for (int i = 0; i < size; i++) {
+            ColumnConstraints columnConstraints = new ColumnConstraints();
+            columnConstraints.setPercentWidth(12.5);
+            board.getColumnConstraints().add(columnConstraints);
+
+            RowConstraints rowConstraints = new RowConstraints();
+            rowConstraints.setPercentHeight(12.5);
+            board.getRowConstraints().add(rowConstraints);
+        }
+
+    }
+}
--- src/main/java/it/units/italiandraughts/ui/BoardController.java
+++ src/main/java/it/units/italiandraughts/ui/BoardController.java
@@ -22,61 +22,22 @@ public class BoardController {
     Label player1Name, player2Name;
 
     public void initialize() {
-        board.setMinSize(
-                ItalianDraughts.getScreenHeight()/3*2,
-                ItalianDraughts.getScreenHeight()/3*2
-        );
-        board.setMaxSize(
-                ItalianDraughts.getScreenHeight()/3*2,
-                ItalianDraughts.getScreenHeight()/3*2
-        );
+
+        Board checkboard = new Board(board);
 
         Stage stage = ItalianDraughts.getPrimaryStage();
         stage.sizeToScene();
         stage.setMinHeight(stage.getHeight());
         stage.setMinWidth(stage.getWidth());
 
+        // add numbers left to the board
         List<Node> rowLabels = rowNumbers.getChildren();
         rowLabels.forEach(e -> {
             ((Label) e).setMaxHeight((ItalianDraughts.getScreenHeight() / 3 * 2) / 8);
             ((Label) e).setMinHeight((ItalianDraughts.getScreenHeight()/3*2)/8);
         });
 
-        final int size = 8;
-        for (int row = 0; row < size; row++) {
-            for (int col = 0; col < size; col++) {
-                StackPane square = new StackPane();
-                String color;
-                if ((row + col) % 2 == 0) {
-                    color = "#d47d35";
-                } else {
-                    color = "white";
-                }
 
-                // TODO just a test, try to add a single piece
-                if (col == 4 && row == 2){
-                    Piece piece = new Piece(board.getMaxHeight()/8);
-                    square.getChildren().addAll(piece.getBaseEllipse());
-                    square.getChildren().add(piece.getUpperEllipse());
-                }
-
-
-
-                square.setStyle("-fx-background-color: " + color + ";");
-                board.add(square, col, row);
-            }
-        }
-
-
-        for (int i = 0; i < size; i++) {
-            ColumnConstraints columnConstraints = new ColumnConstraints();
-            columnConstraints.setPercentWidth(12.5);
-            board.getColumnConstraints().add(columnConstraints);
-
-            RowConstraints rowConstraints = new RowConstraints();
-            rowConstraints.setPercentHeight(12.5);
-            board.getRowConstraints().add(rowConstraints);
-        }
 
 
     }
