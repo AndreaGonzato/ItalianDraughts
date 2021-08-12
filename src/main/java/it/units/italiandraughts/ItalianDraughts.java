80a3d6ee70c0f3689529c427b8a73d8a83d7b004
Tommaso Fonda <fondatommaso2@gmail.com>
2021-08-12 12:03:07 +0200

    ItalianDraughts: move screen{Width,Height} initialization out of start()

--- src/main/java/it/units/italiandraughts/ItalianDraughts.java
+++ src/main/java/it/units/italiandraughts/ItalianDraughts.java
@@ -3,27 +3,21 @@ package it.units.italiandraughts;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
-import javafx.scene.paint.Color;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.util.Objects;
 
 public class ItalianDraughts extends Application {
 
-    private static int screenWidth;
-    private static int screenHeight;
+    private static int screenWidth = (int) Screen.getPrimary().getBounds().getWidth();
+    private static int screenHeight = (int) Screen.getPrimary().getBounds().getHeight();
 
     @Override
     public void start(Stage stage) throws IOException {
-        Rectangle2D screenBounds = Screen.getPrimary().getBounds();
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("ui/MenuLayout.fxml"));
-        screenWidth = (int) screenBounds.getWidth();
-        screenHeight = (int) screenBounds.getHeight();
         Scene scene = new Scene(fxmlLoader.load(), screenWidth / 4, screenHeight / 4);
         stage.setTitle("ItalianDraughts");
         stage.getIcons().add(new Image(getClass().getResourceAsStream("ui/img/icon.png")));
