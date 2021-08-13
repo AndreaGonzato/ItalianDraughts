def7801ef41a209d8cc1e6323be882df08cc4494
Andrea Gonzato <gonzatoandrea98@gmail.com>
2021-08-13 11:02:15 +0200

    test: refactor of BoardTest

--- src/test/java/it/units/italiandraughts/logic/BoardTest.java
+++ src/test/java/it/units/italiandraughts/logic/BoardTest.java
@@ -19,31 +19,36 @@ public class BoardTest {
         board.move(board.getBoard()[5][7], 6, 4);
 
         Piece[][] expectedPieces = new Piece[8][8];
-        expectedPieces[0][0] = new Piece(PieceType.PLAYER2, 0, 0);
-        expectedPieces[0][2] = new Piece(PieceType.PLAYER2, 2, 0);
-        expectedPieces[0][4] = new Piece(PieceType.PLAYER2, 4, 0);
-        expectedPieces[0][6] = new Piece(PieceType.PLAYER2, 6, 0);
-        expectedPieces[1][1] = new Piece(PieceType.PLAYER2, 1, 1);
-        expectedPieces[1][3] = new Piece(PieceType.PLAYER2, 3, 1);
-        expectedPieces[1][5] = new Piece(PieceType.PLAYER2, 5, 1);
-        expectedPieces[1][7] = new Piece(PieceType.PLAYER2, 7, 1);
-        expectedPieces[2][0] = new Piece(PieceType.PLAYER2, 0, 2);
-        expectedPieces[2][2] = new Piece(PieceType.PLAYER2, 2, 2);
-        expectedPieces[2][4] = new Piece(PieceType.PLAYER2, 4, 2);
-        expectedPieces[2][6] = new Piece(PieceType.PLAYER2, 6, 2);
 
-        expectedPieces[5][1] = new Piece(PieceType.PLAYER1,1,5);
-        expectedPieces[5][3] = new Piece(PieceType.PLAYER1,3,5);
-        expectedPieces[5][5] = new Piece(PieceType.PLAYER1,5,5);
-        expectedPieces[4][6] = new Piece(PieceType.PLAYER1,6,4);
-        expectedPieces[6][0] = new Piece(PieceType.PLAYER1,0,6);
-        expectedPieces[6][2] = new Piece(PieceType.PLAYER1,2,6);
-        expectedPieces[6][4] = new Piece(PieceType.PLAYER1,4,6);
-        expectedPieces[6][6] = new Piece(PieceType.PLAYER1,6,6);
-        expectedPieces[7][1] = new Piece(PieceType.PLAYER1,1,7);
-        expectedPieces[7][3] = new Piece(PieceType.PLAYER1,3,7);
-        expectedPieces[7][5] = new Piece(PieceType.PLAYER1, 5, 7);
-        expectedPieces[7][7] = new Piece(PieceType.PLAYER1, 7, 7);
+        addPiece(expectedPieces, PieceType.PLAYER2, 0, 0);
+        addPiece(expectedPieces, PieceType.PLAYER2, 2, 0);
+        addPiece(expectedPieces, PieceType.PLAYER2, 4, 0);
+        addPiece(expectedPieces, PieceType.PLAYER2, 6, 0);
+
+        addPiece(expectedPieces, PieceType.PLAYER2, 1, 1);
+        addPiece(expectedPieces, PieceType.PLAYER2, 3, 1);
+        addPiece(expectedPieces, PieceType.PLAYER2, 5, 1);
+        addPiece(expectedPieces, PieceType.PLAYER2, 7, 1);
+
+        addPiece(expectedPieces, PieceType.PLAYER2, 0, 2);
+        addPiece(expectedPieces, PieceType.PLAYER2, 2, 2);
+        addPiece(expectedPieces, PieceType.PLAYER2, 4, 2);
+        addPiece(expectedPieces, PieceType.PLAYER2, 6, 2);
+
+        addPiece(expectedPieces, PieceType.PLAYER1, 1, 5);
+        addPiece(expectedPieces, PieceType.PLAYER1, 3, 5);
+        addPiece(expectedPieces, PieceType.PLAYER1, 5, 5);
+        addPiece(expectedPieces, PieceType.PLAYER1, 6, 4);
+
+        addPiece(expectedPieces, PieceType.PLAYER1, 0, 6);
+        addPiece(expectedPieces, PieceType.PLAYER1, 2, 6);
+        addPiece(expectedPieces, PieceType.PLAYER1, 4, 6);
+        addPiece(expectedPieces, PieceType.PLAYER1, 6, 6);
+
+        addPiece(expectedPieces, PieceType.PLAYER1, 1, 7);
+        addPiece(expectedPieces, PieceType.PLAYER1, 3, 7);
+        addPiece(expectedPieces, PieceType.PLAYER1, 5, 7);
+        addPiece(expectedPieces, PieceType.PLAYER1, 7, 7);
 
         Assertions.assertArrayEquals(expectedPieces, board.getBoard());
 
@@ -51,31 +56,40 @@ public class BoardTest {
 
     private Piece[][] setUpBoard(){
         Piece[][] pieces = new Piece[8][8];
-        pieces[0][0] = new Piece(PieceType.PLAYER2, 0, 0);
-        pieces[0][2] = new Piece(PieceType.PLAYER2, 2, 0);
-        pieces[0][4] = new Piece(PieceType.PLAYER2, 4, 0);
-        pieces[0][6] = new Piece(PieceType.PLAYER2, 6, 0);
-        pieces[1][1] = new Piece(PieceType.PLAYER2, 1, 1);
-        pieces[1][3] = new Piece(PieceType.PLAYER2, 3, 1);
-        pieces[1][5] = new Piece(PieceType.PLAYER2, 5, 1);
-        pieces[1][7] = new Piece(PieceType.PLAYER2, 7, 1);
-        pieces[2][0] = new Piece(PieceType.PLAYER2, 0, 2);
-        pieces[2][2] = new Piece(PieceType.PLAYER2, 2, 2);
-        pieces[2][4] = new Piece(PieceType.PLAYER2, 4, 2);
-        pieces[2][6] = new Piece(PieceType.PLAYER2, 6, 2);
+        addPiece(pieces, PieceType.PLAYER2, 0, 0);
+        addPiece(pieces, PieceType.PLAYER2, 2, 0);
+        addPiece(pieces, PieceType.PLAYER2, 4, 0);
+        addPiece(pieces, PieceType.PLAYER2, 6, 0);
 
-        pieces[5][1] = new Piece(PieceType.PLAYER1,1,5);
-        pieces[5][3] = new Piece(PieceType.PLAYER1,3,5);
-        pieces[5][5] = new Piece(PieceType.PLAYER1,5,5);
-        pieces[5][7] = new Piece(PieceType.PLAYER1,7,5);
-        pieces[6][0] = new Piece(PieceType.PLAYER1,0,6);
-        pieces[6][2] = new Piece(PieceType.PLAYER1,2,6);
-        pieces[6][4] = new Piece(PieceType.PLAYER1,4,6);
-        pieces[6][6] = new Piece(PieceType.PLAYER1,6,6);
-        pieces[7][1] = new Piece(PieceType.PLAYER1,1,7);
-        pieces[7][3] = new Piece(PieceType.PLAYER1,3,7);
-        pieces[7][5] = new Piece(PieceType.PLAYER1, 5, 7);
-        pieces[7][7] = new Piece(PieceType.PLAYER1, 7, 7);
+        addPiece(pieces, PieceType.PLAYER2, 1, 1);
+        addPiece(pieces, PieceType.PLAYER2, 3, 1);
+        addPiece(pieces, PieceType.PLAYER2, 5, 1);
+        addPiece(pieces, PieceType.PLAYER2, 7, 1);
+
+        addPiece(pieces, PieceType.PLAYER2, 0, 2);
+        addPiece(pieces, PieceType.PLAYER2, 2, 2);
+        addPiece(pieces, PieceType.PLAYER2, 4, 2);
+        addPiece(pieces, PieceType.PLAYER2, 6, 2);
+
+        addPiece(pieces, PieceType.PLAYER1, 1, 5);
+        addPiece(pieces, PieceType.PLAYER1, 3, 5);
+        addPiece(pieces, PieceType.PLAYER1, 5, 5);
+        addPiece(pieces, PieceType.PLAYER1, 7, 5);
+
+        addPiece(pieces, PieceType.PLAYER1, 0, 6);
+        addPiece(pieces, PieceType.PLAYER1, 2, 6);
+        addPiece(pieces, PieceType.PLAYER1, 4, 6);
+        addPiece(pieces, PieceType.PLAYER1, 6, 6);
+
+        addPiece(pieces, PieceType.PLAYER1, 1, 7);
+        addPiece(pieces, PieceType.PLAYER1, 3, 7);
+        addPiece(pieces, PieceType.PLAYER1, 5, 7);
+        addPiece(pieces, PieceType.PLAYER1, 7, 7);
         return pieces;
+    }
+
+    private void addPiece(Piece[][] pieces, PieceType pieceType, int x, int y){
+        Piece piece = new Piece(pieceType, x, y);
+        pieces[y][x] = piece;
     }
 }
