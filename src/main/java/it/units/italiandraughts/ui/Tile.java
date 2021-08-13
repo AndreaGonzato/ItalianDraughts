fbbb4bab949aada7c4d060428886c5a5da2219bc
Tommaso Fonda <fondatommaso2@gmail.com>
2021-08-13 10:11:37 +0200

    ui: Tile: move TileType inside Tile and add initial support for highlighting

--- src/main/java/it/units/italiandraughts/ui/Tile.java
+++ src/main/java/it/units/italiandraughts/ui/Tile.java
@@ -4,4 +4,24 @@ import javafx.scene.layout.StackPane;
 
 public class Tile extends StackPane {
 
+    boolean highlighted;
+    TileType type;
+    private static final String HIGHLIGHT_COLOR = "#56db39";
+
+    public Tile(TileType type) {
+        this.type = type;
+        this.setStyle("-fx-background-color: " + type.getHex() + ";");
+    }
+
+    void highlight() {
+        String newColor;
+        if (highlighted) {
+            newColor = this.type.getHex();
+        } else {
+            newColor = HIGHLIGHT_COLOR;
+        }
+        highlighted = !highlighted;
+        this.setStyle("-fx-background-color: " + newColor + ";");
+    }
+
 }
