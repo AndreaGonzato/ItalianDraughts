c3f8431b94a54e20ae6ad134c165ef2b29b286af
Andrea Gonzato <gonzatoandrea98@gmail.com>
2021-08-26 10:42:45 +0200

    refactor a message exception

--- src/main/java/it/units/italiandraughts/exception/IllegalPositionDrawingException.java
+++ src/main/java/it/units/italiandraughts/exception/IllegalPositionDrawingException.java
@@ -3,7 +3,7 @@ package it.units.italiandraughts.exception;
 public class IllegalPositionDrawingException extends RuntimeException{
 
     public IllegalPositionDrawingException() {
-        super("You can not draw in this position");
+        super("Can not draw in this position");
     }
 
     public IllegalPositionDrawingException(String message) {
