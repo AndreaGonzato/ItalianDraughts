fbc9df86fb6f4ef73fa8259f85c61c1f54809aaf
Andrea Gonzato <gonzatoandrea98@gmail.com>
2021-08-27 11:14:20 +0200

    rename of field in Square

--- src/main/java/it/units/italiandraughts/ui/Square.java
+++ src/main/java/it/units/italiandraughts/ui/Square.java
@@ -8,12 +8,12 @@ public class Square extends StackPane {
     private final Tile tile;
     private final SquareType type;
     private static final String HIGHLIGHT_COLOR = "#56db39";
-    private boolean isHighlighted;
+    private boolean highlighted;
 
     public Square(Tile tile, SquareType type) {
         this.tile = tile;
         this.type = type;
-        this.isHighlighted = false;
+        this.highlighted = false;
         this.setStyle("-fx-background-color: " + type.getHex() + ";");
         tile.setSquare(this);
     }
@@ -23,11 +23,11 @@ public class Square extends StackPane {
     }
 
     public boolean isHighlighted() {
-        return isHighlighted;
+        return highlighted;
     }
 
     void setHighlight(boolean value) {
-        isHighlighted = value;
+        highlighted = value;
         String newColor = value ? HIGHLIGHT_COLOR : this.type.getHex();
         this.setStyle("-fx-background-color: " + newColor + ";");
     }
