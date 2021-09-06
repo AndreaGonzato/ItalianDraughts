71bbcb6ee85b86d48704e4c271685a2006895187
Tommaso Fonda <fondatommaso2@gmail.com>
2021-09-06 11:23:23 +0200

    logic: BlackTile: several improvements
        * Critical bug fix: when placing/removing a piece on/from a tile,
          update the piece's tile field to reflect the change.
        * Add equals() and hashCode(), we'll need them soon.

--- src/main/java/it/units/italiandraughts/logic/BlackTile.java
+++ src/main/java/it/units/italiandraughts/logic/BlackTile.java
@@ -2,6 +2,7 @@ package it.units.italiandraughts.logic;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class BlackTile extends Tile {
 
@@ -15,6 +16,20 @@ public class BlackTile extends Tile {
     BlackTile(int x, int y, Piece piece) {
         super(x, y);
         this.piece = piece;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        BlackTile blackTile = (BlackTile) o;
+        return Objects.equals(piece, blackTile.piece) && Objects.equals(neighbors, blackTile.neighbors);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), piece);
     }
 
     public Map<String, BlackTile> getNeighbors() {
@@ -46,9 +61,11 @@ public class BlackTile extends Tile {
 
     void placePiece(Piece piece) {
         this.piece = piece;
+        piece.setTile(this);
     }
 
     void removePiece() {
+        piece.setTile(null);
         this.piece = null;
     }
 
