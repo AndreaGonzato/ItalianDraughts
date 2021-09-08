3f67570233db7c36a95e5949bdabbda368a94e00
Tommaso Fonda <fondatommaso2@gmail.com>
2021-09-08 11:37:37 +0200

    logic: Graph: implement addEatingEdges()
        * Recursively adds edges for single eatings and chains
          of multiple eatings as well
    
        * Replace two nested for loops with functional programming
          while I'm at it

--- src/main/java/it/units/italiandraughts/logic/Graph.java
+++ src/main/java/it/units/italiandraughts/logic/Graph.java
@@ -10,10 +10,14 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import static it.units.italiandraughts.logic.StaticUtil.matrixToStream;
 
 public class Graph {
 
+    public static final float EATING_KING_MULTIPLIER = 1.2f;
     SimpleDirectedWeightedGraph<Tile, Edge> graph;
     Tile source;
     List<Tile> possibleDestinations;
@@ -26,20 +30,38 @@ public class Graph {
     }
 
     private void addVertices(Board board){
-        for (int i = 0; i < Board.SIZE; i++) {
-            for (int j = 0; j < Board.SIZE; j++) {
-                Tile tile = board.getTiles()[i][j];
-                if ((tile.getX() + tile.getY()) % 2 == 0){
-                    graph.addVertex(tile);
-                }
-            }
-        }
+        matrixToStream(board.getTiles()).filter(tile -> tile instanceof BlackTile)
+                .forEach(graph::addVertex);
     }
 
     void addEdge(Tile source, Tile target, double weight){
         Edge e1 = graph.addEdge(source, target);
         graph.setEdgeWeight(e1, weight);
         possibleDestinations.add(target);
+    }
+
+    void addEatingEdges(Piece eatingPiece, Piece eatenPiece, int step) {
+        BlackTile landingTile = eatingPiece.getPositionAfterEating(eatenPiece);
+        if (landingTile == null) {
+            return;
+        }
+        double weight = 2;
+        if (eatenPiece.getPieceType().equals(PieceType.KING)) {
+            weight += 1f / step;
+        }
+        if (eatingPiece.getPieceType().equals(PieceType.KING)) {
+            weight *= EATING_KING_MULTIPLIER;
+        }
+        addEdge(eatingPiece.getTile(), landingTile, weight);
+        landingTile.placePiece(new Piece(eatingPiece.getPieceColor(),
+                eatingPiece.getPieceType(), landingTile));
+        List<BlackTile> eatableNeighbors = landingTile.getPiece().getNeighborsThisPieceCanMoveTowards()
+                .filter(neighbor -> landingTile.getPiece().canEatNeighbor(neighbor.getPiece()))
+                .collect(Collectors.toList());
+        for (BlackTile neighbor : eatableNeighbors) {
+            addEatingEdges(landingTile.getPiece(), neighbor.getPiece(), ++step);
+        }
+        landingTile.removePiece();
     }
 
     void explorePossibleMoves() {
