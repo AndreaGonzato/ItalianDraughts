0d3940ad24002802828550879f43e3ced148705a
Tommaso Fonda <fondatommaso2@gmail.com>
2021-09-13 15:48:06 +0200

    Re-apply "WIP: disable modules, implement TestFX"
    This reverts commit 2569a7cee3c0212c068414e0d7e5d20a67651d00.

--- src/test/java/it/units/italiandraughts/logic/BoardTest.java
+++ src/test/java/it/units/italiandraughts/logic/BoardTest.java
@@ -3,7 +3,11 @@ package it.units.italiandraughts.logic;
 import it.units.italiandraughts.ui.PieceColor;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.testfx.framework.junit5.ApplicationExtension;
 
+
+@ExtendWith(ApplicationExtension.class)
 public class BoardTest {
 
 
--- src/test/java/it/units/italiandraughts/logic/GameTest.java
+++ src/test/java/it/units/italiandraughts/logic/GameTest.java
@@ -1,9 +1,12 @@
 package it.units.italiandraughts.logic;
 
 import it.units.italiandraughts.ui.PieceColor;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.testfx.assertions.api.Assertions;
+import org.testfx.framework.junit5.ApplicationExtension;
 
+@ExtendWith(ApplicationExtension.class)
 public class GameTest {
 
     @Test
@@ -12,14 +15,7 @@ public class GameTest {
 
         Player actualActivePlayer = game.getActivePlayer();
 
-        Assertions.assertEquals(PieceColor.WHITE, actualActivePlayer.getPieceColor());
+        Assertions.assertThat(actualActivePlayer.getPieceColor()).isEqualTo(PieceColor.WHITE);
     }
-
-    @Test
-    void move(){
-        Game game = new Game(new Board(), new Player("", PieceColor.WHITE), new Player("", PieceColor.BLACK));
-    }
-
-
 
 }
