af1a95e9d0abdf0d375da5ff69b24a955b457b76
Andrea Gonzato <gonzatoandrea98@gmail.com>
2021-10-06 11:05:15 +0200

    remove some unused import

--- src/main/java/it/units/italiandraughts/logic/StaticUtil.java
+++ src/main/java/it/units/italiandraughts/logic/StaticUtil.java
@@ -1,12 +1,6 @@
 package it.units.italiandraughts.logic;
 
-import org.jgrapht.GraphPath;
-
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Stream;
 
 public class StaticUtil {
