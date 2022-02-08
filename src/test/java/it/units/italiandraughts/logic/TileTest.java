8523b2d675b6966ead7507daf21b022f6070f9d0
Tommaso Fonda <fondatommaso2@gmail.com>
2022-02-08 13:08:25 +0100

    test: logic: TileTest: test tile generator

--- src/test/java/it/units/italiandraughts/logic/TileTest.java
+++ src/test/java/it/units/italiandraughts/logic/TileTest.java
@@ -1,11 +1,32 @@
 package it.units.italiandraughts.logic;
 
+import it.units.italiandraughts.logic.tile.BlackTile;
+import it.units.italiandraughts.logic.tile.Tile;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static it.units.italiandraughts.logic.tile.Tile.areValidCoordinatesBiPredicate;
 
 public class TileTest {
+
+    @Test
+    void generateBlackTile() {
+        int x = 0, y = 0;
+        BlackTile expected = new BlackTile(x, y);
+        Tile actual = Tile.generateTile(x, y);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void generateTile() {
+        int x = 1, y = 0;
+        Tile expected = new Tile(x, y);
+        Tile actual = Tile.generateTile(x, y);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
     @Test
     void areValidCoordinatesBiPredicateX0Y0() {
 
