c4f6ca8847730a15522f90c217ade39df3f75f9e
Andrea Gonzato <gonzatoandrea98@gmail.com>
2022-02-08 14:12:40 +0100

    new test moveToReachableBlackTileByEatingPiece

--- src/main/java/it/units/italiandraughts/logic/piece/EatenPiece.java
+++ src/main/java/it/units/italiandraughts/logic/piece/EatenPiece.java
@@ -7,7 +7,7 @@ public class EatenPiece {
     private final Piece piece;
     private final BlackTile position;
 
-    EatenPiece(Piece piece, BlackTile position) {
+    public EatenPiece(Piece piece, BlackTile position) {
         this.piece = piece;
         this.position = position;
     }
@@ -29,4 +29,21 @@ public class EatenPiece {
         return position;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof EatenPiece)) return false;
+
+        EatenPiece that = (EatenPiece) o;
+
+        if (piece != null ? !piece.equals(that.piece) : that.piece != null) return false;
+        return position != null ? position.equals(that.position) : that.position == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = piece != null ? piece.hashCode() : 0;
+        result = 31 * result + (position != null ? position.hashCode() : 0);
+        return result;
+    }
 }
--- src/test/java/it/units/italiandraughts/logic/PieceTest.java
+++ src/test/java/it/units/italiandraughts/logic/PieceTest.java
@@ -1,11 +1,14 @@
 package it.units.italiandraughts.logic;
 
 import it.units.italiandraughts.logic.piece.BlackPiece;
+import it.units.italiandraughts.logic.piece.EatenPiece;
 import it.units.italiandraughts.logic.piece.Piece;
 import it.units.italiandraughts.logic.piece.WhitePiece;
 import it.units.italiandraughts.logic.tile.BlackTile;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
 
 public class PieceTest {
 
@@ -20,6 +23,36 @@ public class PieceTest {
         actualPiece.moveToReachableBlackTile(BlackTile.asBlackTile(board.getTiles()[1][1]));
 
         Assertions.assertEquals(actualPiece.getBlackTile(), BlackTile.asBlackTile(board.getTiles()[1][1]));
+    }
+
+    @Test
+    void moveToReachableBlackTileByEatingPiece(){
+        Board board = Board.reset();
+        board.removePieces();
+
+        Piece pieceToMove = new BlackPiece();
+        BlackTile.asBlackTile(board.getTiles()[0][0]).placePiece(pieceToMove);
+
+        Piece pieceToEat = new WhitePiece();
+        BlackTile.asBlackTile(board.getTiles()[1][1]).placePiece(pieceToEat);
+        EatenPiece eatenPiece = new EatenPiece(pieceToEat, BlackTile.asBlackTile(board.getTiles()[1][1]));
+
+        Optional<EatenPiece> actualEatenPiece = pieceToMove.moveToReachableBlackTile(BlackTile.asBlackTile(board.getTiles()[2][2]));
+
+        Assertions.assertEquals(actualEatenPiece.get(), eatenPiece);
+    }
+
+    @Test
+    void move(){
+        Board board = Board.reset();
+        board.removePieces();
+
+        Piece actualPiece = new BlackPiece();
+        BlackTile.asBlackTile(board.getTiles()[0][0]).placePiece(actualPiece);
+
+        actualPiece.move(BlackTile.asBlackTile(board.getTiles()[3][3]));
+
+        Assertions.assertEquals(actualPiece.getBlackTile(), BlackTile.asBlackTile(board.getTiles()[3][3]));
     }
 
     @Test
