c2eb90bbca8780a1543c4c0ff77e04fd19bcd98d
Tommaso Fonda <fondatommaso2@gmail.com>
2022-02-08 14:47:42 +0100

    test: logic: GameTest: add test for moveAndLog

--- src/test/java/it/units/italiandraughts/logic/GameTest.java
+++ src/test/java/it/units/italiandraughts/logic/GameTest.java
@@ -1,10 +1,15 @@
 package it.units.italiandraughts.logic;
 
+import it.units.italiandraughts.logic.piece.BlackPiece;
+import it.units.italiandraughts.logic.piece.Piece;
+import it.units.italiandraughts.logic.tile.BlackTile;
 import it.units.italiandraughts.ui.PieceColor;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.testfx.assertions.api.Assertions;
+import org.junit.jupiter.api.Assertions;
 import org.testfx.framework.junit5.ApplicationExtension;
+
+import java.util.List;
 
 @ExtendWith(ApplicationExtension.class)
 public class GameTest {
@@ -12,10 +17,26 @@ public class GameTest {
     @Test
     void firstActivePlayerPieceColor(){
         Game game = new Game(new Player("", PieceColor.WHITE), new Player("", PieceColor.BLACK));
-
         Player activePlayer = game.getActivePlayer();
 
-        Assertions.assertThat(activePlayer.getPieceColor()).isEqualTo(PieceColor.WHITE);
+        Assertions.assertEquals(activePlayer.getPieceColor(), PieceColor.WHITE);
+    }
+
+    @Test
+    void moveAndLog() {
+        Game game = new Game(new Player("", PieceColor.WHITE), new Player("", PieceColor.BLACK));
+        Piece piece = new BlackPiece();
+        BlackTile source = new BlackTile(2, 2);
+        source.placePiece(piece);
+        BlackTile destination = new BlackTile(3, 3);
+        Move expectedMove = new Move(piece, List.of(destination));
+        expectedMove.make();
+        Board board = Board.reset();
+        BlackTile actualSource = BlackTile.asBlackTile(board.getTiles()[2][2]);
+        BlackTile actualDestination = BlackTile.asBlackTile(board.getTiles()[3][3]);
+        Move actualMove = game.moveAndLog(actualSource.getPiece(), List.of(actualDestination));
+
+        Assertions.assertEquals(expectedMove, actualMove);
     }
 
 }
