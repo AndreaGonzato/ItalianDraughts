321144d609fb16b7379ec852aa59f86ee673b843
Tommaso Fonda <fondatommaso2@gmail.com>
2022-02-16 13:32:37 +0100

    test: logic: add empty GraphTest class

---
+++ src/test/java/it/units/italiandraughts/logic/GraphTest.java
@@ -0,0 +1,11 @@
+package it.units.italiandraughts.logic;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.Assertions;
+import org.testfx.framework.junit5.ApplicationExtension;
+
+@ExtendWith(ApplicationExtension.class)
+public class GraphTest {
+
+}
